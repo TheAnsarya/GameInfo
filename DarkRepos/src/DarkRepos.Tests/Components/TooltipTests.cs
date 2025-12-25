240d0cc010f3976942e58b77322113f17647b2ca
Andy Hubbard <me@ansarya.com>
2025-12-25 00:43:14 -0500

    feat(web): Add Toast, ConfirmDialog components and Search keyboard nav
    - Add Toast component with success/error/warning/info variants
    - Add ToastContainer for managing multiple notifications
    - Add ConfirmDialog with danger/warning/info types
    - Add keyboard navigation to Search page (arrow keys, Enter, Escape)
    - Add Search.razor.css with selected state styling
    - Add --z-toast z-index variable
    - Add 28 new component tests (ToastTests, TooltipTests, BreadcrumbsTests)
    
    Tests: 230 passing (+28)

---
+++ DarkRepos/src/DarkRepos.Tests/Components/TooltipTests.cs
@@ -0,0 +1,117 @@
+using Bunit;
+using DarkRepos.Web.Components.Shared;
+using FluentAssertions;
+using Xunit;
+
+namespace DarkRepos.Tests.Components;
+
+public class TooltipTests : TestContext {
+	[Fact]
+	public void Tooltip_RendersChildContent() {
+		// Arrange
+		const string buttonText = "Hover me";
+
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip text")
+			.AddChildContent($"<button>{buttonText}</button>"));
+
+		// Assert
+		cut.Markup.Should().Contain(buttonText);
+	}
+
+	[Fact]
+	public void Tooltip_RendersTooltipText() {
+		// Arrange
+		const string tooltipText = "This is helpful information";
+
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, tooltipText)
+			.AddChildContent("<button>Button</button>"));
+
+		// Assert
+		cut.Find(".tooltip").TextContent.Should().Be(tooltipText);
+	}
+
+	[Fact]
+	public void Tooltip_HasCorrectPositionClass_ForTop() {
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip")
+			.Add(p => p.Position, Tooltip.TooltipPosition.Top)
+			.AddChildContent("<span>Content</span>"));
+
+		// Assert
+		cut.Find(".tooltip").ClassList.Should().Contain("top");
+	}
+
+	[Fact]
+	public void Tooltip_HasCorrectPositionClass_ForBottom() {
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip")
+			.Add(p => p.Position, Tooltip.TooltipPosition.Bottom)
+			.AddChildContent("<span>Content</span>"));
+
+		// Assert
+		cut.Find(".tooltip").ClassList.Should().Contain("bottom");
+	}
+
+	[Fact]
+	public void Tooltip_HasCorrectPositionClass_ForLeft() {
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip")
+			.Add(p => p.Position, Tooltip.TooltipPosition.Left)
+			.AddChildContent("<span>Content</span>"));
+
+		// Assert
+		cut.Find(".tooltip").ClassList.Should().Contain("left");
+	}
+
+	[Fact]
+	public void Tooltip_HasCorrectPositionClass_ForRight() {
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip")
+			.Add(p => p.Position, Tooltip.TooltipPosition.Right)
+			.AddChildContent("<span>Content</span>"));
+
+		// Assert
+		cut.Find(".tooltip").ClassList.Should().Contain("right");
+	}
+
+	[Fact]
+	public void Tooltip_DefaultsToTopPosition() {
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip")
+			.AddChildContent("<span>Content</span>"));
+
+		// Assert
+		cut.Find(".tooltip").ClassList.Should().Contain("top");
+	}
+
+	[Fact]
+	public void Tooltip_HasProperAriaAttributes() {
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip text")
+			.AddChildContent("<span>Content</span>"));
+
+		// Assert
+		cut.Find(".tooltip").GetAttribute("role").Should().Be("tooltip");
+	}
+
+	[Fact]
+	public void Tooltip_WrapperHasTabIndex() {
+		// Act
+		var cut = RenderComponent<Tooltip>(parameters => parameters
+			.Add(p => p.Text, "Tooltip")
+			.AddChildContent("<span>Content</span>"));
+
+		// Assert
+		cut.Find(".tooltip-wrapper").GetAttribute("tabindex").Should().Be("0");
+	}
+}
