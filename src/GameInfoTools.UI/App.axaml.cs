a55e4ea618d84c1d75d50f686efabd00f63b2966
Andy Hubbard <me@ansarya.com>
2026-01-09 18:11:12 -0500

    style: Remove trailing whitespace in App.axaml.cs

--- src/GameInfoTools.UI/App.axaml.cs
+++ src/GameInfoTools.UI/App.axaml.cs
@@ -29,7 +29,7 @@ public partial class App : Application {
 		Services.ConfigureProjectServices();
 
 		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-			// Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+			// Avoid duplicate validations from both Avalonia and the CommunityToolkit.
 			// More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
 			DisableAvaloniaDataAnnotationValidation();
 
